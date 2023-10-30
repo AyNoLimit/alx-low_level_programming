4493f6d84afd7914d7f6cd61042f396ac9aae96a
AyNoLimit <ayman.1998@outlook.fr>
2023-10-30 16:21:00 -0700

    Fiiiiix

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -3,14 +3,16 @@
 /**
  * main - Entry point
  * Description : idk
+ * _puts: _puts Prints a string to the standard output
  * @str : str is the member
  * Return: Always 0 (Success)
  */
-
 void _puts(char *str);
+
 {
 int i = 0;
-while (str[i])
+while
+(str[i]);
 {
 _putchar(str[i]);
 i++;
